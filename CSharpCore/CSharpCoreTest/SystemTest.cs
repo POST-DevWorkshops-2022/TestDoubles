99423acdcf84f374043323c9c68d9ea174f0700a
Katrin Heiderer <katrin.heiderer@nagarro.com>
2022-12-01 15:25:31 +0100

    initial commit

---
+++ CSharpCore/CSharpCoreTest/SystemTest.cs
@@ -0,0 +1,34 @@
+using System;
+using Xunit;
+using FluentAssertions;
+
+namespace CSharpCore.Test
+{
+    public class SystemTest
+    {
+        [Fact]
+        public void newlyCreatedSystem_hasNoLoggedInUsers()
+        {
+        }
+
+        [Fact]
+        public void oneUserIsLoggedIn_afterSuccessfulAuthorization()
+        {
+        }
+
+        [Fact]
+        public void errorMailIsSent_onFailedLoginAttempt()
+        {
+        }
+
+        [Fact]
+        public void errorMailIsSent_forInvalidUserName()
+        {
+        }
+
+        [Fact]
+        public void knownUser_canLogin()
+        {
+        }
+    }
+}
